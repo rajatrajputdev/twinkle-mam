a511f99ae80a328857c5e67464ec47413f9d0ef6
rajatuiwebdev <hello.rajat.rajput@gmail.com>
2025-02-23 12:06:06 +0530

    fixed

--- main.py
+++
@@ -1,9 +0,0 @@
-
-r = 0
-for i in range(10):
-    r = r + 1
-    print(f"""                    <div class="blog-item bg-white rounded wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="blog-img">
-                            <img src="awards/{r}.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                        </div>
-                    </div>""")
